b3e4a351b5be06d55d186cec1775cebbd49ce49b
Kaden-Billin <kbillin@uwyo.edu>
2024-10-17 22:10:52 +0000

    what

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -1,8 +1,8 @@
-# Your Name Here
+# Kaden Billin
 # UWYO COSC 1010
-# Submission Date
+# 10-17-24
 # Lab 06
-# Lab Section: 
+# Lab Section: 12
 # Sources, people worked with, help given to: 
 # your
 # comments
@@ -70,6 +70,138 @@ print(len(random_string)) # Print out the size for reference
 # Output which letter occurred the most 
 # Output which letter occurred the least 
 # Output what the percentage of the string each character is, again in alphabetical
+letter_values = {}
+aoccurances = 0
+boccurances = 0
+coccurances = 0
+doccurances = 0
+eoccurances = 0
+foccurances = 0
+goccurances = 0
+hoccurances = 0
+ioccurances = 0
+joccurances = 0
+koccurances = 0
+loccurances = 0
+moccurances = 0
+noccurances = 0
+ooccurances = 0
+poccurances = 0
+qoccurances = 0
+roccurances = 0
+soccurances = 0
+toccurances = 0
+uoccurances = 0
+voccurances = 0
+woccurances = 0
+xoccurances = 0
+yoccurances = 0
+zoccurances = 0
+
+for letter in random_string:
+    if letter == "a":
+        aoccurances = (aoccurances+1)
+        letter_values["a occurances"] = aoccurances
+for letter in random_string:
+    if letter == "b":
+        boccurances = (boccurances+1)
+        letter_values["b occurances"] = boccurances
+for letter in random_string:
+    if letter == "c":
+        coccurances = (coccurances+1)
+        letter_values["c occurances"] = coccurances
+for letter in random_string:
+    if letter == "d":
+        doccurances = (doccurances+1)
+        letter_values["d occurances"] = doccurances
+for letter in random_string:
+    if letter == "e":
+        eoccurances = (eoccurances+1)
+        letter_values["e occurances"] = eoccurances
+for letter in random_string:
+    if letter == "f":
+        foccurances = (foccurances+1)
+        letter_values["f occurances"] = foccurances
+for letter in random_string:
+    if letter == "g":
+        goccurances = (goccurances+1)
+        letter_values["g occurances"] = goccurances
+for letter in random_string:
+    if letter == "h":
+        hoccurances = (hoccurances+1)
+        letter_values["h occurances"] = hoccurances
+for letter in random_string:
+    if letter == "i":
+        ioccurances = (ioccurances+1)
+        letter_values["i occurances"] = ioccurances
+for letter in random_string:
+    if letter == "j":
+        joccurances = (joccurances+1)
+        letter_values["j occurances"] = joccurances
+for letter in random_string:
+    if letter == "k":
+        koccurances = (koccurances+1)
+        letter_values["k occurances"] = koccurances
+for letter in random_string:
+    if letter == "l":
+        loccurances = (loccurances+1)
+        letter_values["l occurances"] = loccurances
+for letter in random_string:
+    if letter == "m":
+        moccurances = (moccurances+1)
+        letter_values["m occurances"] = moccurances
+for letter in random_string:
+    if letter == "n":
+        noccurances = (noccurances+1)
+        letter_values["n occurances"] = noccurances
+for letter in random_string:
+    if letter == "o":
+        ooccurances = (ooccurances+1)
+        letter_values["o occurances"] = ooccurances
+for letter in random_string:
+    if letter == "p":
+        poccurances = (poccurances+1)
+        letter_values["p occurances"] = poccurances
+for letter in random_string:
+    if letter == "q":
+        qoccurances = (qoccurances+1)
+        letter_values["q occurances"] = qoccurances
+for letter in random_string:
+    if letter == "r":
+        roccurances = (roccurances+1)
+        letter_values["r occurances"] = roccurances
+for letter in random_string:
+    if letter == "s":
+        soccurances = (soccurances+1)
+        letter_values["s occurances"] = soccurances
+for letter in random_string:
+    if letter == "t":
+        toccurances = (toccurances+1)
+        letter_values["t occurances"] = toccurances
+for letter in random_string:
+    if letter == "u":
+        uoccurances = (uoccurances+1)
+        letter_values["u occurances"] = uoccurances
+for letter in random_string:
+    if letter == "v":
+        voccurances = (voccurances+1)
+        letter_values["v occurances"] = voccurances
+for letter in random_string:
+    if letter == "w":
+        woccurances = (woccurances+1)
+        letter_values["w occurances"] = woccurances
+for letter in random_string:
+    if letter == "x":
+        xoccurances = (xoccurances+1)
+        letter_values["x occurances"] = xoccurances
+for letter in random_string:
+    if letter == "y":
+        yoccurances = (yoccurances+1)
+        letter_values["y occurances"] = yoccurances
+for letter in random_string:
+    if letter == "z":
+        zoccurances = (zoccurances+1)
+        letter_values["z occurances"] = zoccurances
 
 #Tips and trick:
 # You can iterate through strings like you would a list
@@ -85,9 +217,13 @@ print(len(random_string)) # Print out the size for reference
 
 # Output: each letter and its corresponding occurrence in alphabetical order
 
+print(letter_values)
 print("*"*75)
+
 # Output which letter occurred the most 
 
+for letter, value in letter_values.items():
+    print(value)
 most_occurred = ""
 least_occurred = ""
 
